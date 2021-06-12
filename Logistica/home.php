b08fc1699437b82a7aef1d4e516f0d52e065b4f4
JoelE7 <jescobarlasalas@alumno.unlam.edu.ar>
2021-06-11 23:34:44 -0300

    Menu horizontal home v2

--- Logistica/home.php
+++ Logistica/home.php
@@ -5,16 +5,29 @@ require_once("barraHome.php");
 ?>
 
 
-    <div class="container-fluid btn-violet">
+    <div class="container-fluid bg-primary">
         <div class="row">
             <div class="col m-0 p-0">
-                <h1 class="text-orange text-center mb-5">Bienvenido nombre de usuario</h1>
-                <h2 class="text-orange mb-5">¿Que operación desea realizar?</h2>
-                <button class="btn btn-orange btn-block mb-5 mt-3">Ver Viajes</button>
-                <button class="btn btn-success btn-block mb-5 mt-3">Ver Services</button>
-                <button class="btn btn-orange btn-block mb-5 mt-3">Ver Vehiculos</button>
-                <button class="btn btn-success btn-block mb-5 mt-3">Ver Empleados</button>
-                <button class="btn btn-orange btn-block mb-5 mt-3">Ver Usuarios</button>
+                <table class="table table-primary table-hover table-responsive-xs">
+                    <tr>
+                        <td>
+                            <button class="btn btn-orange btn-block mb-5 mt-3">Ver Viajes</button>
+                        </td>
+                        <td>
+                            <button class="btn btn-violet btn-block mb-5 mt-3">Ver Services</button>
+                        </td>
+                        <td>
+                            <button class="btn btn-success btn-block mb-5 mt-3">Ver Vehiculos</button>
+                        </td>
+                        <td>
+                            <button class="btn btn-violet btn-block mb-5 mt-3">Ver Empleados</button>
+                        </td>
+                        <td>
+                            <button class="btn btn-orange btn-block mb-5 mt-3">Ver Usuarios</button>
+                        </td>
+                    </tr>
+                </table>
+
             </div>
         </div>
     </div>
